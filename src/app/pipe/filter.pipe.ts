52237998be8a8ed0621f770b40c12701b2ccca69
kvgabi4 <kvgabi4@gmail.com>
2021-03-16 23:43:22 +0100

    pipe

--- src/app/pipe/filter.pipe.ts
+++ src/app/pipe/filter.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: any[], phrase: string, key: string = ''): any {
+  transform(value: any[] | null, phrase: string, key: string = ''): any {
 
     if (!Array.isArray(value) || !phrase || !key) {
       return value;
